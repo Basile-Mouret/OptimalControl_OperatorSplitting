ec98682e24475ff3a7157a84f825aa7b624f445c
Basile <145753507+Basile-Mouret@users.noreply.github.com>
2026-03-09 16:42:52 +0100

    FIX: Name and Readme

--- src/Operator_Spliting_Optimal_Control.jl
+++
@@ -1,6 +0,0 @@
-module Operator_Spliting_Optimal_Control
-
-greet() = println("Hello World!")
-export greet
-
-end # module Operator_Spliting_Optimal_Control
---
+++ src/OptimalControl_OperatorSplitting.jl
@@ -0,0 +1,6 @@
+module OptimalControl_OperatorSplitting
+
+greet() = println("Hello World!")
+export greet
+
+end # module OptimalControl_OperatorSplitting
